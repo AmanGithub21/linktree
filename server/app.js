37bb65b118d1628d5cad3f16b50438db55b9f038
Aman Porwal <01amanporwal@gmail.com>
2024-07-22 23:09:00 +0530

    config render

--- server/app.js
+++ server/app.js
@@ -25,10 +25,10 @@ mongoose
   });
 
 app.use(express.json());
-app.use(express.static(path.join(__dirname, "/client/build")));
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "/client/build", "index.html"));
-});
+// app.use(express.static(path.join(__dirname, "/build")));
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(__dirname, "/build", "index.html"));
+// });
 
 app.use("/account", authRoutes);
 app.use("/linktree", linktreeRoutes);
